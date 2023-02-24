0c4ea03ecb63051c2c8929145385b908c09833bd
VAndrMin <126180592+VAndrMin@users.noreply.github.com>
2023-02-24 05:05:38 +0300

    Add bot start

--- bot_main_body.py
+++ bot_main_body.py
@@ -1,2 +1,9 @@
 import telebot
 
+bot = telebot.TeleBot("5729519893:AAEp364nJgQEROZt4UoZHRxNtvq2G5r-CvM")
+
+@bot.message_handler(commands=['start'])
+def send_welcome(massage) :
+    bot.reply_to(massage, "Вітання в гів бот тесті")
+
+bot.polling()
