19976a6218fda4d07fd669b998f342d1ba8cf9a3
VAndrMin <126180592+VAndrMin@users.noreply.github.com>
2023-02-24 17:21:15 +0300

    add simple bot responder

---
+++ config.py
@@ -0,0 +1,10 @@
+TOKEN = "5729519893:AAEp364nJgQEROZt4UoZHRxNtvq2G5r-CvM"
+
+CHANNELS = [
+    ["Test channel 1", "https://t.me/task_test_channel1", "id:-1001805771484"],
+    ["Test channel 2", "https://t.me/task_test_channel2", "id:-1001763595562"],
+    ["Test channel 3", "https://t.me/task_test_channel3", "id:-1001626957248"],
+    ["Test channel 4", "https://t.me/task_test_channel4", "id:-1001800227165"],
+]
+
+NOT_SUB_MESSAGE = "Для участі в giveaway необхідно підписатись на канали!"
